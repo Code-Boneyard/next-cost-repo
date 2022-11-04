e0984f8affbad3c706ae014fe7e18dd16abd6259
Max Geller <mxgeller@gmail.com>
2022-11-04 16:56:42 -0700

    adding navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Main.module.css';
+import { Navbar } from '../components/Navbar';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Navbar />
         <h1 className="text-3xl font-bold underline">This is the main page.</h1>
       </main>
     </div>
