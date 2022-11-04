2586f318004bf1ff15da6ac3c1adb23380c231d6
Max Geller <mxgeller@gmail.com>
2022-11-04 16:48:22 -0700

    adding html and body

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,24 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Main.module.css';
 
+
 export default function Home() {
   return (
     <html className="h-full bg-gray-100">
-      <div>
-        <Head>
-          <title>MEP Systems Cost Repository</title>
-          <meta name="description" content="MEP Systems Cost Repository" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>MEP Systems Cost Repository</title>
+        <meta name="description" content="MEP Systems Cost Repository" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main>
-          <h1 className="text-3xl font-bold underline">This is the main page.</h1>
-        </main>
-      </div>
+      <body className="h-full">
+        <div className="min-h-full">
+
+          <main>
+            <h1 className="text-3xl font-bold underline">This is the main page.</h1>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
