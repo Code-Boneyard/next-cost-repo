65e1646ca846a9d209ea971c75b7ae3a2b33fa37
Max Geller <mxgeller@gmail.com>
2022-11-04 16:51:59 -0700

    fixed html message

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Main.module.css';
 
-
 export default function Home() {
   return (
-    <html className="h-full bg-gray-100">
+    <div>
       <Head>
         <title>MEP Systems Cost Repository</title>
         <meta name="description" content="MEP Systems Cost Repository" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <body className="h-full">
-        <div className="min-h-full">
-
-          <main>
-            <h1 className="text-3xl font-bold underline">This is the main page.</h1>
-          </main>
-        </div>
-      </body>
-    </html>
+      <main>
+        <h1 className="text-3xl font-bold underline">This is the main page.</h1>
+      </main>
+    </div>
   );
 }
