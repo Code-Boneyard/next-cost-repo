2065acddfd4ee1adf2f054a4e91e0affae4a6e3d
Max Geller <mxgeller@gmail.com>
2022-11-04 18:00:22 -0700

    updating profile

--- pages/sessions/profile.tsx
+++ pages/sessions/profile.tsx
@@ -11,7 +11,15 @@ const Profile: NextPageWithLayout = () => {
           <h3 className="text-3xl font-bold tracking-tight text-gray-900 uppercase">Profile</h3>
         </div>
       </header>
-      <p>Come here to update your profile</p>
+      <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className="px-4 py-6 sm:px-0">
+              <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+            </div>
+            {/* /End replace */}
+          </div>
+        </main>
     </div>
   );
 };
