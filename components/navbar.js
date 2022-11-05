d00335c835a5d5c549750c1b7b43eaeaddaca351
Max Geller <mxgeller@gmail.com>
2022-11-04 19:42:09 -0700

    configuring navbar

--- components/navbar.js
+++ components/navbar.js
@@ -49,13 +49,6 @@ export const Navbar = () => {
             </Link>{' '}
           </div>
           <div className="ml-4 flex items-center md:ml-6">
-            <button
-              type="button"
-              className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span className="sr-only">View notifications</span>
-              <BellIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
