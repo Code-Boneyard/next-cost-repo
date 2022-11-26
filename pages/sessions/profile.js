d3f26614b15bdd6e36115f0c6b0fb4e067468831
Max Geller <mxgeller@gmail.com>
2022-11-25 17:44:45 -0700

    adding tailwind list box to profile

--- pages/sessions/profile.js
+++ pages/sessions/profile.js
@@ -1,16 +1,30 @@
 import Header from './../../components/Header';
 import { PaperClipIcon } from '@heroicons/react/20/solid';
+import { Fragment, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+
+const people = [
+  { id: 1, name: 'Pacific Standard Time (PST)' },
+  { id: 2, name: 'Mountain Standard Time (MST)' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 export default function Profile() {
+  const [selected, setSelected] = useState(people[1]);
   return (
     <div>
-      <Header title="profile" />
+      <Header title="My Profile" />
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 ">
           <div className="px-4 py-6 sm:px-0 ">
             <div className="">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">Applicant Information</h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details</p>
+              <h3 className="text-lg font-medium leading-6 text-gray-900">Information</h3>
             </div>
             <div className="mt-5 border-t border-gray-200">
               <dl className="divide-y divide-gray-200">
@@ -22,7 +36,7 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
@@ -35,7 +49,7 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
@@ -48,20 +62,63 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
                 </div>
-                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+                <div className="py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
                   <dt className="text-sm font-medium text-gray-500">Timezone</dt>
-                  <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    <span className="flex-grow">Mountain Standard Time (MST)</span>
+                  <dd className="mt-0 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <span >
+                      <Listbox value={selected} onChange={setSelected}>
+                        {({ open }) => (
+                          <>
+                            <div className="relative mt-0">
+                              <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+                                <span className="block truncate">{selected.name}</span>
+                                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                  <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                </span>
+                              </Listbox.Button>
+
+                              <Transition show={open} as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+                                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                  {people.map((person) => (
+                                    <Listbox.Option
+                                      key={person.id}
+                                      className={({ active }) =>
+                                        classNames(active ? 'text-white bg-indigo-600' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9')
+                                      }
+                                      value={person}>
+                                      {({ selected, active }) => (
+                                        <>
+                                          <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>{person.name}</span>
+
+                                          {selected ? (
+                                            <span className={classNames(active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4')}>
+                                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                            </span>
+                                          ) : null}
+                                        </>
+                                      )}
+                                    </Listbox.Option>
+                                  ))}
+                                </Listbox.Options>
+                              </Transition>
+                            </div>
+                          </>
+                        )}
+                      </Listbox>
+                    </span>
+
+                    <span className="flex-grow"></span>
+
                     <span className="ml-4 flex-shrink-0">
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
@@ -74,7 +131,7 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
@@ -87,7 +144,7 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
@@ -100,7 +157,7 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
@@ -113,7 +170,7 @@ export default function Profile() {
                       <button
                         type="button"
                         className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Edit
                       </button>
                     </span>
                   </dd>
