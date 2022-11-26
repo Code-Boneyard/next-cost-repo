bfcec6f7ec9197f64b3e082dc115b5be2751dfcb
Max Geller <mxgeller@gmail.com>
2022-11-25 17:34:28 -0700

    profile page development

--- pages/sessions/profile.js
+++ pages/sessions/profile.js
@@ -10,12 +10,12 @@ export default function Profile() {
           <div className="px-4 py-6 sm:px-0 ">
             <div className="">
               <h3 className="text-lg font-medium leading-6 text-gray-900">Applicant Information</h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+              <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details</p>
             </div>
             <div className="mt-5 border-t border-gray-200">
               <dl className="divide-y divide-gray-200">
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
-                  <dt className="text-sm font-medium text-gray-500">Full name</dt>
+                  <dt className="text-sm font-medium text-gray-500">Full Name</dt>
                   <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                     <span className="flex-grow">Max Geller</span>
                     <span className="ml-4 flex-shrink-0">
@@ -30,7 +30,7 @@ export default function Profile() {
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
                   <dt className="text-sm font-medium text-gray-500">Email</dt>
                   <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    <span className="flex-grow">Backend Developer</span>
+                    <span className="flex-grow">mgeller@laytonconstruction.com</span>
                     <span className="ml-4 flex-shrink-0">
                       <button
                         type="button"
@@ -41,9 +41,9 @@ export default function Profile() {
                   </dd>
                 </div>
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
-                  <dt className="text-sm font-medium text-gray-500">Email address</dt>
+                  <dt className="text-sm font-medium text-gray-500">Mobile</dt>
                   <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    <span className="flex-grow">margotfoster@example.com</span>
+                    <span className="flex-grow">Timezone</span>
                     <span className="ml-4 flex-shrink-0">
                       <button
                         type="button"
@@ -54,9 +54,9 @@ export default function Profile() {
                   </dd>
                 </div>
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
-                  <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
+                  <dt className="text-sm font-medium text-gray-500">Timezone</dt>
                   <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    <span className="flex-grow">$120,000</span>
+                    <span className="flex-grow">Mountain Standard Time (MST)</span>
                     <span className="ml-4 flex-shrink-0">
                       <button
                         type="button"
@@ -67,12 +67,48 @@ export default function Profile() {
                   </dd>
                 </div>
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
-                  <dt className="text-sm font-medium text-gray-500">About</dt>
+                  <dt className="text-sm font-medium text-gray-500">Start Page</dt>
                   <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    <span className="flex-grow">
-                      Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla
-                      mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                    <span className="flex-grow">Projects</span>
+                    <span className="ml-4 flex-shrink-0">
+                      <button
+                        type="button"
+                        className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Update
+                      </button>
                     </span>
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+                  <dt className="text-sm font-medium text-gray-500">Theme</dt>
+                  <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <span className="flex-grow">Light</span>
+                    <span className="ml-4 flex-shrink-0">
+                      <button
+                        type="button"
+                        className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Update
+                      </button>
+                    </span>
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+                  <dt className="text-sm font-medium text-gray-500">Password</dt>
+                  <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <span className="flex-grow">Change Password</span>
+                    <span className="ml-4 flex-shrink-0">
+                      <button
+                        type="button"
+                        className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Update
+                      </button>
+                    </span>
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+                  <dt className="text-sm font-medium text-gray-500">Role</dt>
+                  <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <span className="flex-grow">Change Password</span>
                     <span className="ml-4 flex-shrink-0">
                       <button
                         type="button"
